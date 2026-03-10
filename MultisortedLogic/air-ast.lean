ae81cb1a1ca27723861f5f2c0bd55513a2efe3fc
rongshuge <shugerong@gmail.com>
2026-03-10 18:24:57 -0400

    polymorphic funcs

--- MultisortedLogic/air-ast.lean
+++ MultisortedLogic/air-ast.lean
@@ -26,6 +26,12 @@ variable {α : AirSorts → Type*}
 open AirSorts
 
 inductive airFunc : List AirSorts → AirSorts → Type
+  -- air-ast :: Const
+  | True : airFunc [] Bool
+  | False : airFunc [] Bool
+  | Nat : (i : String) → airFunc [] Poly
+  | BitVector : (bits : List Nat) → (width: UInt32) → airFunc [] (BitVec width)
+
   -- air-ast :: Binop
   | Implies : airFunc [Bool, Bool] Bool
   | Eq : airFunc [Int, Int] Bool
@@ -57,6 +63,7 @@ inductive airFunc : List AirSorts → AirSorts → Type
   -- | BitSignExtend (fr: UInt32)
 
   -- air-ast :: MultiOp
+  -- TODO
   | And : (n : Nat) → airFunc (List.replicate n Bool) Bool
   | Or : (n : Nat) → airFunc (List.replicate n Bool) Bool
   | Xor : (n : Nat) → airFunc (List.replicate n Bool) Bool
@@ -89,3 +96,71 @@ inductive airFunc : List AirSorts → AirSorts → Type
   | ofB : airFunc [Bool] Poly  -- B
   | toI : airFunc [Poly] Int   -- %I
   | toB : airFunc [Poly] Bool  -- %B
+
+
+def air_ast : MSLanguage AirSorts := {
+  Functions := airFunc
+  Relations := fun _ => Empty
+}
+
+open airFunc
+
+abbrev addZFunc : air_ast.Functions [Int, Int] Int := Add
+abbrev subZFunc : air_ast.Functions [Int, Int] Int := Sub
+abbrev mulZFunc : air_ast.Functions [Int, Int] Int := Mul
+abbrev divZFunc : air_ast.Functions [Int, Int] Int := EuclideanDiv
+abbrev modZFunc : air_ast.Functions [Int, Int] Int := EuclideanMod
+
+abbrev andBFunc (n : Nat) : air_ast.Functions (List.replicate n Bool) Bool := And n
+
+instance {α : AirSorts → Type*} : Add (MSLanguage.air_ast.Term α Int) :=
+{ add := addZFunc.apply₂ }
+
+theorem addZ_def (α : AirSorts → Type*) (t₁ t₂ : MSLanguage.air_ast.Term α Int) :
+    t₁ + t₂ = addZFunc.apply₂ t₁ t₂ := rfl
+
+instance {α : AirSorts → Type*} : Sub (MSLanguage.air_ast.Term α Int) :=
+{ sub := subZFunc.apply₂ }
+
+theorem subZ_def (α : AirSorts → Type*) (t₁ t₂ : MSLanguage.air_ast.Term α Int) :
+    t₁ - t₂ = subZFunc.apply₂ t₁ t₂ := rfl
+
+instance {α : AirSorts → Type*} : Mul (MSLanguage.air_ast.Term α Int) :=
+{ mul := mulZFunc.apply₂ }
+
+theorem mulZ_def (α : AirSorts → Type*) (t₁ t₂ : MSLanguage.air_ast.Term α Int) :
+    t₁ * t₂ = mulZFunc.apply₂ t₁ t₂ := rfl
+
+instance {α : AirSorts → Type*} : Div (MSLanguage.air_ast.Term α Int) :=
+{ div := divZFunc.apply₂ }
+
+theorem divZ_def (α : AirSorts → Type*) (t₁ t₂ : MSLanguage.air_ast.Term α Int) :
+    t₁ / t₂ = divZFunc.apply₂ t₁ t₂ := rfl
+
+instance {α : AirSorts → Type*} : Mod (MSLanguage.air_ast.Term α Int) :=
+{ mod := modZFunc.apply₂ }
+
+theorem modZ_def (α : AirSorts → Type*) (t₁ t₂ : MSLanguage.air_ast.Term α Int) :
+    t₁ % t₂ = modZFunc.apply₂ t₁ t₂ := rfl
+
+instance {α : AirSorts → Type*} : AndOp (MSLanguage.air_ast.Term α Bool) :=
+{ and := (andBFunc 2).apply₂ }
+
+-- theorem andB_def (α : AirSorts → Type*) (n : Nat) (t₁ t₂ : MSLanguage.air_ast.Term α Bool) :
+--     (andBFunc n).apply₂ t₁ t₂ = := rfl
+
+/-- Making this an abbrev instead of a def makes Lean automatically unfold this,
+which helps with typeclass inference -/
+abbrev AirMod (B I F P T BV : Type u) : AirSorts → Type _
+  | AirSorts.Bool => B
+  | AirSorts.Int => I
+  | AirSorts.Fun => F
+  | AirSorts.Named "Poly" => P
+  | AirSorts.Named "Type" => T
+  | AirSorts.BitVec w => BV
+  | _ => sorry
+
+
+end MSLanguage
+
+end MSFirstOrder
